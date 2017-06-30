195ed61f324e8f01c934ab6192d92f689d63bbbe
emmanuel olowu <emmanuel olowu>
2017-06-30 14:51:59 +0100

    nn

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+        	'name' => 'Merchant',
+        ]);
+
+        DB::table('roles')->insert([
+        	'name' => 'User',
+        ]);  
+
+        DB::table('roles')->insert([
+        	'name' => 'Admin',
+        ]);  
+
+        DB::table('roles')->insert([
+        	'name' => 'SuperAdminau',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| 
 */
 
-Route::get('/', 'Auth\UserController@index')->name('index');
+Route::get('/', 'Auth\UserController@index')->name('index'); 
 
 Route::get('/users/follow/more', 'Auth\UserController@index')->name('index');
 
