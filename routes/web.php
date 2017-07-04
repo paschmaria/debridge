ae53922aac265795f4e9f66e1fcb7526bcd7af6a
nurudeen <ajayinurudeen998@gmail.com>
2017-07-04 19:58:39 +0100

    changes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-| 
+| 104.131.168.249
 */
 
 Route::get('/', 'Auth\UserController@index')->name('index'); 
