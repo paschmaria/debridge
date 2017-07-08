2ae82619896ee88b3f41bfbc4fd55ae7dc798324
nurudeen <ajayinurudeen998@gmail.com>
2017-07-08 23:18:31 +0100

    fixes on notifications and followers

--- resources/views/users/followers_bridger.blade.php
+++ resources/views/users/followers_bridger.blade.php
@@ -66,13 +66,13 @@
 			        				</div>
 			        				<div class="col-md-5 col-sm-5 col-xs-5">
 			        				@if(in_array($user->id, $following_ids))
-			        					<form method="post" action="{{ route('unfollow', $user->reference) }}">
+			        					{{-- <form method="post" action="{{ route('unfollow', $user->reference) }}"> --}}
 			        						<button class="btn unfollow btn-sm f-14 waves-light waves-effect c-brand btn-outline-brand m-t-40 m-b-50" data-email="{{$user->reference}}" data-id="{{$user->id}}" data-fname="{{$user->full_name()}}" ><span class="fa fa-check">&nbsp; &nbsp;</span>Unfollow</button>
-			        					</form>
+			        					{{-- </form> --}}
 			        				@else
-			        					<form method="post" action="{{ route('follow', $user->reference) }}">
-			        						<button class="btn unfollow btn-sm f-14 waves-light waves-effect c-brand btn-outline-brand m-t-40 m-b-50" data-email="{{$user->reference}}" data-id="{{$user->id}}" data-fname="{{$user->full_name()}}" ><span class="fa fa-check">&nbsp; &nbsp;</span>Follow</button>
-			        					</form>
+			        					{{-- <form method="post" action="{{ route('follow', $user->reference) }}"> --}}
+			        						<button class="btn follow btn-sm f-14 waves-light waves-effect c-brand btn-outline-brand m-t-40 m-b-50" data-email="{{$user->reference}}" data-id="{{$user->id}}" data-fname="{{$user->full_name()}}" ><span class="fa fa-check">&nbsp; &nbsp;</span>Follow</button>
+			        					{{-- </form> --}}
 			        				@endif
 			        				</div>
 		        				</div>
