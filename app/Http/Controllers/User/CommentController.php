843a91695e3a000c75f7a688484750575f823843
nurudeen <ajayinurudeen998@gmail.com>
2017-07-21 16:27:04 +0100

    changes

--- app/Http/Controllers/User/CommentController.php
+++ app/Http/Controllers/User/CommentController.php
@@ -48,11 +48,11 @@ class CommentController extends Controller
             'user_id' => auth()->user()->id,
             'post_id' => $post->id
             ]);
-        return response()->json([
-            'success' => $comment
-        ]);    
-        // $request->session()->flash('success', 'Comment succesful');
-        // return back();
+        // return response()->json([
+        //     'success' => $comment
+        // ]);    
+        $request->session()->flash('success', 'Comment succesful');
+        return back();
     }
 
     /**
