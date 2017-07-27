e29da004da991a9bf92cda3bdc0b943446393ae1
emmanuel olowu <emmanuelr20@yahoo.com>
2017-07-27 15:27:45 +0100

    migration and seeder for produc categories

--- resources/views/users/user_tradeline.blade.php
+++ resources/views/users/user_tradeline.blade.php
@@ -32,7 +32,17 @@
             @if($user->checkRole())
                 <div class="row m-t-10">
                     <div class="col-sm-9">
-                        <h1 class="h1-responsive f-48 text-center m-t-20 m-b-5 c-brand w-500">{{ strtoupper($user->full_name()) }}</h1>
+                        <h1 class="h1-responsive f-48 text-center m-t-20 m-b-5 c-brand w-500">{{ strtoupper($user->full_name()) }}
+                            @if(auth()->user()->id != $user->id)
+                                @if(in_array($user, auth()->user()->following->toArray()))
+                                <a href="{{ route('follow', $user->reference) }}">
+                                    <button class="btn btn-sm btn-outline-brand follow" data-email="{{ $user->reference }}">follow></button>
+                                </a>
+                                @else
+                                    <button class="btn btn-sm btn-brand unfollow" data-email="{{ $user->reference }}"> unfollow </button>
+                                @endif
+                            @endif
+                        </h1>
                         @if($user->community)
                             <p class="text-center">{{ $user->community_address() }}<small class="c-gray"> (Trade Community)</small></p>
                         @endif
