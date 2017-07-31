e426463d92d816207e54eff5fea20725fe2a2c67
nurudeen <ajayinurudeen998@gmail.com>
2017-07-31 18:08:36 +0100

    changes

--- resources/views/market/partials/timeline.blade.php
+++ resources/views/market/partials/timeline.blade.php
@@ -37,7 +37,7 @@
                                         <!--Indicators-->
                                         <ol class="carousel-indicators">
                                             @for ($i = 0; $i < count($post->pictures->images); $i++)
-                                                <li data-target="#myCarousel{{ $post->id }}" data-slide-to="{{ $i }}" @if($i === 0 )class="active"@endif></li>
+                                                <li data-target="#myCarousel{{ $post->id }}" data-slide-to="{{ $i }}" @if($i === 0 ) class="active"@endif></li>
                                             @endfor
                                         </ol>
                                         <!--/.Indicators-->
