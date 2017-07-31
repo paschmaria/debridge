10bd8b8c5e1058189d5d209e6f4187ca53048c49
emmanuel olowu <emmanuelr20@yahoo.com>
2017-07-31 17:41:53 +0200

    post using ajx

--- resources/views/market/partials/buttons/unadmire.blade.php
+++
@@ -1,6 +0,0 @@
-<button type="button" class="btn bg-white m-l-3 f-14 m-r-5 f-14 width-200">
-    <span class="unadmire">Unadmire
-        <small class="c-gray f-10">({{ $post->admires->count() }})</small>&nbsp;
-    </span>
-    <i class="fa fa-heart"></i>
-</button>
