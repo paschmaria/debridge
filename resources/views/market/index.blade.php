c8cb4291a112743a9c63a30215b6212f1134f562
SWITCHNG <info@switch.ng>
2017-09-09 09:20:55 +0100

    Fix top menu

--- resources/views/market/index.blade.php
+++ resources/views/market/index.blade.php
@@ -21,12 +21,13 @@
                         <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('followers', auth()->user()->reference) }}">Followers</a></li> -->
                         <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('timeline', auth()->user()->reference) }}">Tradeline</a></li>
                         <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('community', auth()->user()->reference) }}">Trade Community</a></li>
-                    @else
+                    <!-- Remove @ else here  essentially debugging for the right time.-->
                         <!--<li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('following', auth()->user()->reference) }}">Following</a></li>
                         <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('followers', auth()->user()->reference) }}">Followers</a></li> -->
                         <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" data-toggle="modal" data-target="#basicExample">Tradeline</a></li>
                         <!--<li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" data-toggle="modal" data-target="#basicExample">Trade Community</a></li> -->
                     @endif
+                    <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" data-toggle="modal" data-target="#basicExample">Tradeline</a></li>
                     <!--<li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('bridge_shops') }}">Bridger Shops</a></li> -->
                     <li class="nav-item m-r-10"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('araha_market') }}">Araha Market</a></li>
                     <li class="nav-item"><a class="nav-link hover-underline text-uppercase mc-white" href="{{ route('exhibition') }}">Exhibition Stand</a></li>
